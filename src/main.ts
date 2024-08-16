b24c69a567cd7dcf9237cca46f2ac7c093470cc7
GandaBruh <suteekran07@gmail.com>
2024-08-16 13:12:48 +0700

    chore: change port and test config

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001', 
+    allowedHeaders: 'Content-Type,Authorization', 
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
-    .setTitle('DataWow Test API')
-    .setDescription('DataWow Test API')
+    .setTitle('API')
+    .setDescription('API')
     .addBearerAuth()
     .setVersion('1.0')
     .build();
